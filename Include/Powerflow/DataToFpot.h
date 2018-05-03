631dd82213f90fc07bf0f7ba663c86414afbe13a
Junjie <junjie.zhang@eonerc.rwth-aachen.de>
2018-05-03 11:56:00 +0200

    minor changes for preperation

---
+++ Include/Powerflow/DataToFpot.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "../../Dependencies/fpotencia/src/fpotencia.h"
+
+#include<vector>
+#include<map>
+
+
+#include <CIMModel.hpp>
+#include <IEC61970.hpp>
+
+#include "cps/Definitions.h"
+#include "cps/Component.h"
+#include "cps/Components.h"
+#include "cps/Node.h"
+#include "cps/Terminal.h"
+#include "cps/Logger.h"
+#include "cps/SystemTopology.h"
+
+
+class FpoTrans{
+private:
+		ComponentBase::Ptr mapbranch(BaseClass* obj)
+		{};
+		void mapGen()
+		{};
+		void mapBranch()
+		{};
+
+
+
+    
+}
---
+++ Source/Powerflow/DataToFpot.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <CIMModel.hpp>
+#include <IEC61970.hpp>
+#include "bus.h"
+#include "fpotencia.h"
+#include "DPsim.h"
+#include "DataToFpot.h"
+#include "../../Dependencies/libcps/Include/cps/CIM/Reader.h"
+#include "../../Dependencies/libcps/Include/cps/systemtopology.h"
+
+using namespace::DPsim;
+using namespace::CIM;
+
+void main()
+{
+
+
+}
+
+
